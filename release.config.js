9ee19fbc0977cc123fd332d99e99798b9a417282
Omid Var <33044916+omidvarahram@users.noreply.github.com>
2025-02-28 14:11:50 +1000

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,38 +1,30 @@
-const commitAnalyzer = await import('@semantic-release/commit-analyzer');
-const releaseNotesGenerator = await import('@semantic-release/release-notes-generator');
-const execPlugin = await import('@semantic-release/exec');
-const changelogPlugin = await import('@semantic-release/changelog');
-const npmPlugin = await import('@semantic-release/npm');
-const gitPlugin = await import('@semantic-release/git');
-const githubPlugin = await import('@semantic-release/github');
-
 export default {
   repositoryUrl: 'https://github.aus.thenational.com/NAB-X/nui-themes.git',
   branches: ['master'],
   extends: 'semantic-release-monorepo',
   plugins: [
-    commitAnalyzer.default,
+    '@semantic-release/commit-analyzer',
     {
       preset: 'angular'
     },
-    releaseNotesGenerator.default,
+    '@semantic-release/release-notes-generator',
     {
-      [execPlugin.default]: {
+      '@semantic-release/exec': {
         successCmd: 'echo ${nextRelease.version} > .next-version.txt'
       }
     },
     {
-      [changelogPlugin.default]: {
+      '@semantic-release/changelog': {
         changelogFile: 'CHANGELOG.md'
       }
     },
-    npmPlugin.default,
+    '@semantic-release/npm',
     {
-      [gitPlugin.default]: {
+      '@semantic-release/git': {
         message: 'chore(release): v${nextRelease.version}',
         assets: ['CHANGELOG.md', 'package.json']
       }
     },
-    githubPlugin.default
+    '@semantic-release/github'
   ]
 };
