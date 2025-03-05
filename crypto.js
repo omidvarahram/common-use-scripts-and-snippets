6b18a3141bcad7e5e9b9531f4f026ddb58494c11
Omid Var <33044916+omidvarahram@users.noreply.github.com>
2025-03-05 16:10:26 +1000

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -1,10 +1,20 @@
 import 'react-native-get-random-values';
 
+// Define the type for crypto to avoid TypeScript errors
+declare global {
+  interface Crypto {
+    getRandomValues<T extends ArrayBufferView | null>(array: T): T;
+  }
+  
+  var crypto: Crypto;
+}
+
+// Manually patch `crypto.getRandomValues` if it doesn't exist
 if (!global.crypto || !global.crypto.getRandomValues) {
+  console.log('Manually patching crypto.getRandomValues...');
+  
   global.crypto = {
     ...global.crypto,
-    getRandomValues: (array) => {
-      return array.map(() => Math.floor(Math.random() * 256));
-    }
-  };
-}
+    getRandomValues: <T extends ArrayBufferView | null>(array: T): T => {
+      if (!array) {
+        throw new Error('crypto
