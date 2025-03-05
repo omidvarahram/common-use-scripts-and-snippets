0a499223fc341e1e939577f7ade1e2772158236e
Omid Var <33044916+omidvarahram@users.noreply.github.com>
2025-03-05 16:08:22 +1000

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -1,14 +1,10 @@
 import 'react-native-get-random-values';
-import { randomBytes } from 'react-native-crypto';
 
 if (!global.crypto || !global.crypto.getRandomValues) {
-  console.log("Manually patching crypto.getRandomValues...");
   global.crypto = {
     ...global.crypto,
-    getRandomValues: (arr) => {
-      const bytes = randomBytes(arr.length);
-      arr.set(bytes);
-      return arr;
+    getRandomValues: (array) => {
+      return array.map(() => Math.floor(Math.random() * 256));
     }
   };
 }
